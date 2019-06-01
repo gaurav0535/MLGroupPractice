7a1403e1fb43e04d803dfdf0d922a1dd668f8bda
ashish-002 <ashish010.yadav@gmail.com>
2019-06-01 21:20:37 +0530

    For & while Loops

---
+++ CodingPractice/Loops.py
@@ -0,0 +1,16 @@
+# For Loops
+# A program to find all numbers divisible by 7 but not a multiple of 5, between 100 and 1000 (both included)
+for i in range(100, 1001):
+    if (i % 7) and (i % 5!= 0):
+    	print(i)
+
+# Table of 12, 22 and 32
+# A program to print table of 12, 22 and 32
+x = int(input())
+i = 1
+while i <= 10:
+	print(x * i)
+	i += 1
+   
+   
+
